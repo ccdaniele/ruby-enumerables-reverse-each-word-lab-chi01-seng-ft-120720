8f9d7dfcc77f61a47799a4b095b28b884e11c8ab
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-29 19:43:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,13 +11,11 @@ end
 
 
   
-  def reverse_each_word(string)
+ def reverse_each_word(string)
 new_array = []
 reverse = []
 new_array = string.split(" ")
 
-new_array.each do |string| 
-  reverse << string.reverse 
-end
-return reverse.join(" ")
-end
+new_array.collect {|string| 
+   string.reverse}
+ end
