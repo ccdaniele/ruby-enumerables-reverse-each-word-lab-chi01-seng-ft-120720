4c9b01dbaad634470c4f0abcf39caf82de6606e4
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-29 18:56:46 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def  reverse_each_word ()
+
+def reverse_each_word(string)
+new_array = []
+reverse = []
+new_array = string.split("")
+reverse = new_array.reverse
+return reverse.join
+
+end
+  
+  
