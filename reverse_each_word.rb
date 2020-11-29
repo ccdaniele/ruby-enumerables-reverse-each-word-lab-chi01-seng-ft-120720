5de7ad7a6f0148a32168b4c413c83e866179d044
Daniel Calderon <danieleduardocald@gmail.com>
2020-11-29 19:17:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,15 @@
-def  reverse_each_word ()
-
 def reverse_each_word(string)
 new_array = []
 reverse = []
 new_array = string.split("")
-reverse = new_array.reverse
-return reverse.join
 
+new_array = ["h", "e", "l", "l", "o", ",", " ", "H", "o", "w", " ", "a", "r", "e", " ", "y", "o", "u", " ", "t", "o", "n", "i", "g", "h", "t", "?"]
+
+new_array.each do |string| 
+  reverse << string.reverse 
 end
+return reverse.reverse
+end
+
   
   
